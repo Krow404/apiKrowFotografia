7e0d20c1c8e4b494489761450f2eb65b1fba9e1f
krow <ehvalero23@gmail.com>
2024-02-16 13:58:26 -0600

    se crea el primer commit

---
+++ src/main/java/com/krow/fotografia/KrowFotografiaApplication.java
@@ -0,0 +1,13 @@
+package com.krow.fotografia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrowFotografiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrowFotografiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krow/fotografia/KrowFotografiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krow.fotografia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrowFotografiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
